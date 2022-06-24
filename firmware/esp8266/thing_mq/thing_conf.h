29fb012f411094030eee129c98ed98a13fac1f23
anuvgupta <me@anuv.me>
2022-06-24 16:57:18 -0500

    spacing

--- firmware/esp8266/thing_mq/thing_conf.h
+++ firmware/esp8266/thing_mq/thing_conf.h
@@ -4,8 +4,8 @@
 #define __T_CONF_H__
 
 // wifi
-#define WIFI_SSID "RajeevM"
-#define WIFI_PASS "1081Corn"
+#define WIFI_SSID "21 Resident"
+#define WIFI_PASS "Hookem*2101"
 
 // device
 #define DEVICE_USER "anuv"
